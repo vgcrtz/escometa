5abb540a46831aa8de609820b491e74b78c2ea04
Omar Jesús Vázquez Sánchez <omar_jv.s@hotmail.com>
2026-04-26 11:16:15 -0600

    Registrar-Vista ya lista, falta agregar la parte superior de ESCOM.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './registro/registro.html',
+  styleUrl: './registro/registro.css'
 })
 export class App {
   protected readonly title = signal('ESCOMETA');
