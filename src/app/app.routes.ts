769307c9c6364ddc2d40748c32566ec9c62a65d4
Kitzia <kitziaara@gmail.com>
2026-04-26 21:46:23 -0600

    Iniciar sesion - Kit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {Registro} from './registro/registro';
+import {Login} from './login/login';
 
 // AGREGAR SUS RUTAS Y EL COMPONENTE
 export const routes: Routes = [
   { path: 'registro', component: Registro },
+  { path: 'iniciar-sesion', component: Login },
 ];
