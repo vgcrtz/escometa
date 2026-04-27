e7595345115751a8b594196e847e0f6b47282e82
Omar Jesús Vázquez Sánchez <omar_jv.s@hotmail.com>
2026-04-27 00:59:50 -0600

    Acomode esto otra vez, espero que funcione, solo cambie los routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {Registro} from './registro/registro';
 import {Login} from './login/login';
+import {Busqueda} from './busqueda/busqueda';
 
 // AGREGAR SUS RUTAS Y EL COMPONENTE
 export const routes: Routes = [
   { path: 'registro', component: Registro },
   { path: 'iniciar-sesion', component: Login },
+  { path: 'busqueda', component: Busqueda}
 ];
